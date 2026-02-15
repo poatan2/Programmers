1b5be804b38823245945bea763e4306a15b79874
poatan2 <128802443+poatan2@users.noreply.github.com>
2026-02-15 15:28:57 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,17 @@
+def solution(id_pw, db):
+    answer = ''
+
+    for i in db:
+        if i[0] == id_pw[0]:
+            if i[1] == id_pw[1]:
+                answer = "login"
+                return answer
+            else:
+                answer = "wrong pw"
+                return answer
+
+        else:
+            answer = "fail"
+    return answer
+
+
