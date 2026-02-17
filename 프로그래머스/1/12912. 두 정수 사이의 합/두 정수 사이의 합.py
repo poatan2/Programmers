15e1457b6ad3a3dbaa86379e355f10036398ed19
poatan2 <128802443+poatan2@users.noreply.github.com>
2026-02-17 22:35:20 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 864.55 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -0,0 +1,17 @@
+def solution(a, b):
+    answer = 0
+    temp = 0
+    
+    if a==b:
+        return a
+    
+    elif a<b:
+        for i in range(a,b+1):
+            answer+= i
+    
+    else:
+        for i in range(b,a+1):
+            answer+= i
+        
+        
+    return answer
