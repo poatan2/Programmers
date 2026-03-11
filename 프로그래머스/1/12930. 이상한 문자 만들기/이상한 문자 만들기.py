c14131334e728f9d542be5a2bb1ddbb08684891e
poatan2 <128802443+poatan2@users.noreply.github.com>
2026-03-11 10:26:16 +0900

    Enhance solution function with comments and formatting
    Refactor solution function to improve readability and add comments.

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,19 +1,17 @@
 def solution(s):
     answer = ''
     
-    loc = 0
+    loc = 0     #자릿수를 나타낼 변수
+    
     for i in s:
-        if i == ' ':
+        if i == ' ':    # 공백이라면 loc를 0으로 초기화
             loc = 0
-            answer += ' '
-        else:
-            if loc%2 == 0:
+            answer += ' '   
+        else:           # 문자가 보이면 대소문자로 바꾸는 로직
+            if loc%2 == 0:  
                 answer+=i.upper()
             else:
                 answer+=i.lower()
             loc +=1
-    
-    
-    
     
     return answer
